395fee8d499fd95aa6c81ee10e729684d491c271
Thomas Dourgarian <tdourg@Thomass-MacBook-Air.local>
2019-10-05 11:58:49 -0500

    input boxes turn red if nothing was put in

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 $(document).ready(readyNow);
 let yearlyCost = 0;
+
 function readyNow() {
-    $("#submitBtn").on("click",addEmployee);
-    $("tbody").on("click",".deleteBtn",deleteEmployee);
-    $("body").on("mouseover","#submitBtn",borderToWhite);
+    $("#submitBtn").on("click", addEmployee);
+    $("tbody").on("click", ".deleteBtn", deleteEmployee);
+    $("body").on("mouseover", "#submitBtn", borderToWhite);
 }
 
 
@@ -14,10 +15,19 @@ function addEmployee() {
     let titleEl = $("#titleInput");
     let salaryEl = $("#salaryInput");
 
-    yearlyCost += Number(salaryEl.val());
-    console.log(yearlyCost);
-    $("table").append(
-        `<tr>
+    checkInputs(firstEl,lastEl,idEl,titleEl,salaryEl);
+
+    if (!firstEl.val() &&
+        !lastEl.val() &&
+        !idEl.val() &&
+        !titleEl.val() &&
+        !salaryEl.val()
+    ) {
+
+        yearlyCost += Number(salaryEl.val());
+        
+        $("table").append(
+            `<tr>
             <td>${firstEl.val()}</td>
             <td>${lastEl.val()}</td>
             <td>${idEl.val()}</td>
@@ -25,7 +35,16 @@ function addEmployee() {
             <td id="annualSalary">${salaryEl.val()}</td>
             <td><button class="deleteBtn">Delete</button></td>
         </tr>`
-    );
+        );
+
+        firstEl.val("");
+        lastEl.val("");
+        idEl.val("");
+        titleEl.val("");
+        salaryEl.val("");
+    }
+
+
 
     checkMonthly(yearlyCost);
 }
@@ -38,19 +57,32 @@ function deleteEmployee() {
 }
 
 function checkMonthly(cost) {
-    if(cost/12 >= 20000) {
-        $("#monthlyCost").css("background-color","red");
+    if (cost / 12 >= 20000) {
+        $("#monthlyCost").css("background-color", "red");
     } else {
-        $("#monthlyCost").css("background","transparent");
+        $("#monthlyCost").css("background", "transparent");
     }
 
-    $("#monthlyCost").text("Monthly Total: $" + cost/12);
+    $("#monthlyCost").text("Monthly Total: $" + cost / 12);
 }
 
 function borderToWhite() {
-    $("#submitBtn").css("border-color","white");
+    $("#submitBtn").css("border-color", "white");
 
-    $("#submitBtn").on("mouseout",function() {
-        $(this).css("border-color"," rgb(78, 206, 78)");
+    $("#submitBtn").on("mouseout", function () {
+        $(this).css("border-color", " rgb(78, 206, 78)");
     });
+}
+
+
+function checkInputs(firstEl,lastEl,idEl,titleEl,salaryEl) {
+    let inputArray = [firstEl,lastEl,idEl,titleEl,salaryEl];
+
+    for (el of inputArray) {
+        if (!el.val()) {
+            el.css("border-color","red");
+        } else {
+            el.css("border-color","rgba(153,236,255,1)");
+        }
+    }
 }
